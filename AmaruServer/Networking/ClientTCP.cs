dce0d17ce6fcf861de261d2ac4962a12349c58a7
Andrea Milanta <andrea.milanta@gmail.com>
2018-05-14 16:27:36 +0200

    ServerTCP and ClientTCP added - TO BE REVIEWED!

---
+++ AmaruServer/Networking/ClientTCP.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+using AmaruServer.Constants;
+
+namespace AmaruServer.Networking
+{
+    class ClientTCP
+    {
+        private int _index;
+        private uint _ip;
+        public Socket socket;
+        private bool _closing = false;
+        private byte[] _buffer = new byte[NetworkConstants.BufferSize];
+
+        public int Index { get => _index; set => _index = value; }
+        public uint Ip { get => _ip; set => _ip = value; }
+        public bool Closing { get => _closing; set => _closing = value; }
+
+        public void startClient()
+        {
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceviceCallback), socket);
+            Closing = false;
+        }
+
+        private void ReceviceCallback(IAsyncResult ar)
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            try
+            {
+                int receivedSize = socket.EndReceive(ar);
+                if (receivedSize <= 0)
+                    CloseClient(_index);
+                else
+                {
+                    byte[] databuffer = new byte[receivedSize];
+                    Array.Copy(_buffer, databuffer, receivedSize);
+                    //Handle data
+
+                }
+            }
+            catch
+            {
+                CloseClient(_index);
+            }
+        }
+
+        public void CloseClient(int index)
+        {
+            _closing = true;
+            Console.WriteLine("Connection from {0} has been terminated", NetworkConstants.ip2str(_ip));
+            socket.Close();
+        }
+    }
+}
---
+++ AmaruServer/Networking/ServerTCP.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+using AmaruServer.Constants;
+
+namespace AmaruServer.Networking
+{
+    abstract class ServerTCP
+    {
+        private Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private byte[] _buffer = new byte[NetworkConstants.BufferSize];
+    
+        //public static Socket ServerSocket { get => _serverSocket; set => _serverSocket = value; }
+
+        /// <summary>
+        /// setup Server properties
+        /// </summary>
+        public void setupServer()
+        {
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, NetworkConstants.ServerPort));
+            _serverSocket.Listen(NetworkConstants.MaxUsers);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            Socket socket = _serverSocket.EndAccept(ar);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+            HandleNewConnection(socket);
+        }
+
+        protected abstract void HandleNewConnection(Socket newSocket);
+    }
+}
