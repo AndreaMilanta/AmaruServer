72282497bb5ce2d98b866be0c8463c11923692dd
Marco Faleri <marco.faleri@studenti.unimi.it>
2018-06-21 13:44:55 +0200

    ai passes

--- source/Networking/AIUser.cs
+++ source/Networking/AIUser.cs
@@ -1,9 +1,13 @@
 ﻿using AmaruCommon.Actions;
 using AmaruCommon.Communication.Messages;
+using AmaruCommon.Constants;
+using AmaruCommon.GameAssets.Cards;
 using AmaruCommon.GameAssets.Players;
+using AmaruCommon.Responses;
 using AmaruServer.Game.Managing;
 using ClientServer.Messages;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
@@ -16,8 +20,15 @@ namespace AmaruServer.Networking
     public class AIUser : User
     {
         MessageHandler messageHandler = null;
+        Queue<Message> listOfActions;
+        LimitedList<LimitedList<CreatureCard>> OtherPlayersOuterField;
+        LimitedList<LimitedList<CreatureCard>> OtherPlayersInnerField;
         public AIUser(string logger) : base(logger)
         {
+            listOfActions = new Queue<Message>();
+            OtherPlayersInnerField = new LimitedList<LimitedList<CreatureCard>>(4);
+            OtherPlayersOuterField = new LimitedList<LimitedList<CreatureCard>>(4);
+
 
         }
 
@@ -30,14 +41,41 @@ namespace AmaruServer.Networking
 
         public override void Write(Message mex)
         {
+            if (mex is ResponseMessage)
+            {
+                Response responseReceived = ((ResponseMessage)mex).Response;
+                if (responseReceived is NewTurnResponse)
+                {
+                    if (((NewTurnResponse)responseReceived).ActivePlayer == AmaruCommon.GameAssets.Characters.CharacterEnum.AMARU)
+                    {
+                        if (!GameManager.IsMainTurn)
+                        {
+                            listOfActions.Enqueue(new ActionMessage(new EndTurnAction(AmaruCommon.GameAssets.Characters.CharacterEnum.AMARU, -1, GameManager.IsMainTurn)));
+                        }
+                        else if (GameManager.IsMainTurn)
+                        {
 
+                            listOfActions.Enqueue(new ActionMessage(new EndTurnAction(AmaruCommon.GameAssets.Characters.CharacterEnum.AMARU, -1, GameManager.IsMainTurn)));
+                            //think()
+                        }
+                    }
+                }
+            }
+        }
+
+        private void think()
+        {
+            foreach(User target in GameManager._userDict.Values)
+            {
+                //inutile prendere l'asEnemy immagino
+                OtherPlayersOuterField.Add(target.Player.AsEnemy.Outer);
+                OtherPlayersInnerField.Add(target.Player.AsEnemy.Inner);
+            }
         }
 
         public override Message ReadSync(int timeout_s)
         {
-            return new ActionMessage(new EndTurnAction(AmaruCommon.GameAssets.Characters.CharacterEnum.AMARU, -1, GameManager.IsMainTurn));
-            Player.PlayACreatureFromHand(Player.Hand[0].Id,AmaruCommon.Constants.Place.OUTER);
-            return null;
+            return listOfActions.Dequeue();
         }
 
         public override void ReadASync(bool continuous)
@@ -49,5 +87,6 @@ namespace AmaruServer.Networking
         {
 
         }
+
     }
 }
