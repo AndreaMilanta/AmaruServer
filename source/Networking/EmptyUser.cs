f420ae2d0af1165ce99e915cacc996b8e9023d22
Andrea Milanta <andrea.milanta@gmail.com>
2018-06-22 01:35:34 +0200

    added GameManager constructor for AI simulation

---
+++ source/Networking/EmptyUser.cs
@@ -0,0 +1,23 @@
+﻿using AmaruCommon.GameAssets.Players;
+using ClientServer.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmaruServer.Networking
+{
+    public class EmptyUser : User
+    {
+        public EmptyUser(Player player, string logger) : base(logger)
+        {
+            this.Player = player;
+        }
+
+        public override void Write(Message mex)
+        {
+        }
+    }
+}
